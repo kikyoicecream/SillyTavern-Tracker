eb8cedb66d3551578cd22cc72fe242d3d2c5c8db
KingAbdul99th <adadreigh@gmail.com>
2025-04-28 22:26:06 +0300

    empty select options before appending

--- src/settings/settings.js
+++ src/settings/settings.js
@@ -198,6 +198,8 @@ function updateConnectionProfileDropdown() {
 	const connectionProfileSelect = $("#tracker_connection_profile");
 	const connectionProfiles = getConnectionProfiles()
 	debug("connections profiles found", connectionProfiles)
+	completionPresetsSelect.empty()
+	connectionProfileSelect.append($("<option>").val("current").text("Same as current"))
 	for (const profileName of connectionProfiles) {
 		const option = $("<option>").val(profileName).text(profileName);
 
@@ -233,6 +235,8 @@ function updateCompletionPresetsDropdown() {
 	const completionPresetsSelect = $("#tracker_completion_preset");
 	const completionPresets = getCompletionPresets()
 	debug("completion presets found", completionPresets)
+	completionPresetsSelect.empty()
+	completionPresets.append($("<option>").val("current").text("Same as current"))
 	for (const presetName of completionPresets) {
 		const option = $("<option>").val(presetName).text(presetName);
 
